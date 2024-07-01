ee792a8ddf87315b894dc9b2bd6c289fcba3dd57
Igor Verkhoshentsev <igor.verkh@gmail.com>
2024-07-02 00:53:15 +0500

    add dependencies

--- app/src/main/java/ru/wb/meetings/ui/theme/Color.kt
+++ app/src/main/java/ru/wb/meetings/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Custom
+val NeutralOffWhite = Color(0xFFF7F7FC)
+
+val BrandDefault = Color(0xFF9A41FE)
+val BrandDark = Color(0xFF660EC8)
+val BrandDarkMode = Color(0xFF8207E8)
+val BrandLight = Color(0xFFECDAFF)
+val BrandBackground = Color(0xFFF5ECFF)
+val NeutralDisabled = Color(0xFFADB5BD)
+val NeutralWeak = Color(0xFFA4A4A4)
+val NeutralActive = Color(0xFF29183B)
+val AvatarBorder = Color(0xFFD2D5F9)
+val SixSixSix = Color(0xFF666666)
+
+val ButtonFocusBorder = Color(0xFFF5ECFF)
