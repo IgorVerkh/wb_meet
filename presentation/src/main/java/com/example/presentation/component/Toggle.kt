9059898c3f1fcff900c74456c05faa4339510492
Igor Verkhoshentsev <igor.verkh@gmail.com>
2024-08-16 14:43:17 +0500

    fix toggle

--- presentation/src/main/java/com/example/presentation/component/Toggle.kt
+++ presentation/src/main/java/com/example/presentation/component/Toggle.kt
@@ -2,53 +2,25 @@ package com.example.presentation.component
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
-import androidx.wear.compose.material.FractionalThreshold
-import androidx.wear.compose.material.rememberSwipeableState
-import androidx.wear.compose.material.swipeable
 import com.example.presentation.theme.BrandDefault
 import com.example.presentation.theme.Disabled
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 @Composable
 internal fun Toggle(
@@ -77,9 +49,13 @@ internal fun Toggle(
     Canvas(
         modifier = modifier
             .size(width = width, height = height)
-            .clickable(
-                enabled = enabled
-            ) { onCheckedChange(checked) }
+            .toggleable(
+                value = checked,
+                enabled = enabled,
+                role = Role.Switch
+            ) {
+                onCheckedChange(checked)
+            }
     ) {
         drawRoundRect(
             color = if (checked) checkedTrackColor else uncheckedTrackColor,
